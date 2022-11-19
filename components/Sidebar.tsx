df5b9499a55615124527d2e80a19fb4aee428971
aldiery <aldieryrgon@gmail.com>
2022-11-19 03:55:05 -0600

    Training Animated plus bugfixes

---
+++ components/Sidebar.tsx
@@ -0,0 +1,29 @@
+type SidebarProps = {
+  handleSetBounds: Function;
+  handleTrain: Function;
+};
+
+const Sidebar: React.FC<SidebarProps> = ({ handleSetBounds, handleTrain }) => {
+  return (
+    <div className="shadow-2x z-10 w-48  bg-[hsl(191,19%,16%)] p-6 text-lg text-white shadow-amber-200">
+      Classifier
+      <button
+        className="rounded bg-[hsl(205,26%,49%)] py-2 px-4 font-bold text-white hover:bg-[hsl(205,26%,25%)]"
+        onClick={() => handleTrain()}
+      >
+        TRAIN!
+      </button>
+      <button
+        className="rounded bg-[hsl(205,26%,49%)] py-2 px-4 font-bold text-white hover:bg-[hsl(205,26%,25%)]"
+        onClick={() => handleSetBounds()}
+      >
+        Set Bound
+      </button>
+      <button className="rounded bg-[hsl(205,26%,49%)] py-2 px-4 font-bold text-white hover:bg-[hsl(205,26%,25%)]">
+        TRAIN!
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
